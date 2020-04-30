ad270db2d3fcfb82e67f6886ff52686c6fe08cce
Rafael Timbó <timbo.rafa@gmail.com>
2020-04-30 01:07:09 -0300

    classifier singleton

---
+++ app/recommendation/classifier_singleton.py
@@ -0,0 +1,59 @@
+from sklearn.metrics.pairwise import sigmoid_kernel
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from sklearn.svm import SVC
+
+from app.services.order_service import OrderService
+
+class ClassifierSingleton:
+    """Singleton to avoid recomputing expensive modeling operations
+    """
+    __classifier = None
+    __le = None
+    __df = None
+
+    @staticmethod
+    def get():
+        key = "recommendation-system"
+        classifier = ClassifierSingleton.__classifier
+
+        if classifier == None:
+            classifier, df, le = ClassifierSingleton.instantiate()
+            ClassifierSingleton.__classifier = classifier
+            ClassifierSingleton.__df = df
+            ClassifierSingleton.__le = le
+
+        return classifier, df, le
+    
+    @staticmethod
+    def instantiate():
+        order_service = OrderService()
+        le = {
+            'client'  : LabelEncoder(),
+            'product' : LabelEncoder(),
+            'variety' : LabelEncoder(),
+            'country' : LabelEncoder(),
+            'category': LabelEncoder(),
+            'vintage' : LabelEncoder(),
+            'key'     : LabelEncoder(),
+        }
+
+        data = {
+            'client'  : le['client']  .fit_transform(order_service .data['client']),
+            'product' : le['product'] .fit_transform(order_service .data['product']),
+            'variety' : le['variety'] .fit_transform(order_service .data['variety']),
+            'country' : le['country'] .fit_transform(order_service .data['country']),
+            'category': le['category'].fit_transform(order_service.data['category']),
+            'vintage' : le['vintage'] .fit_transform(order_service .data['vintage']),
+            'key'     : le['key']     .fit_transform(order_service.data['key'])
+        }
+
+        df = pd.DataFrame(data=data, columns=list(data.keys()))
+
+        X = df.drop('key', axis=1)
+        Y = df['key']
+
+        svclassifier = SVC(kernel='sigmoid')
+        svclassifier.fit(X, Y)
+
+        return svclassifier, df, le
--- app/recommendation/recommendation.py
+++ app/recommendation/recommendation.py
@@ -5,42 +5,14 @@ from sklearn.svm import SVC
 
 from app.services.order_service import OrderService
 from app.services.client_fix import get_client_id
+from app.recommendation.classifier_singleton import ClassifierSingleton
 
 def recommend(cliente):
     """ Recommend products to a client with Support Vector Machine(SVM)
     using a sigmoid kernel.
-    """    
+    """
 
-    cliente = get_client_id(cliente)
-
-    order_service = OrderService()
-    le = {
-        'client'  : LabelEncoder(),
-        'product' : LabelEncoder(),
-        'variety' : LabelEncoder(),
-        'country' : LabelEncoder(),
-        'category': LabelEncoder(),
-        'vintage' : LabelEncoder(),
-        'key'     : LabelEncoder(),
-    }
-
-    data = {
-        'client'  : le['client']  .fit_transform(order_service .data['client']),
-        'product' : le['product'] .fit_transform(order_service .data['product']),
-        'variety' : le['variety'] .fit_transform(order_service .data['variety']),
-        'country' : le['country'] .fit_transform(order_service .data['country']),
-        'category': le['category'].fit_transform(order_service.data['category']),
-        'vintage' : le['vintage'] .fit_transform(order_service .data['vintage']),
-        'key'     : le['key']     .fit_transform(order_service.data['key'])
-    }
-
-    df = pd.DataFrame(data=data, columns=list(data.keys()))
-    
-    X = df.drop('key', axis=1)
-    Y = df['key']
-
-    svclassifier = SVC(kernel='sigmoid')
-    svclassifier.fit(X, Y)
+    svclassifier, df, le = ClassifierSingleton.get()
 
     client_le = le['client'].transform([cliente])[0]
     x_client = df[df['client'] == client_le].drop('key', axis=1)
